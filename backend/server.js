01819aaea88caf8c96d62b354072850004c6d742
hyasiel <hyasiel1@gmail.com>
2026-01-25 22:19:39 -0500

    add connections

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,21 @@ const express = require("express");
 const path = require("path");
 const port = 3000;
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/api", (req, res) => {});
 
 app.post("/login", (req, res) => {});
 
-app.post("/register", (req, res) => {});
+app.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+  console.log(name);
+  res.send("register endpoint");
+});
 
 app.get("/protected", (req, res) => {});
 
--- frontend/src/login/Signup.tsx
+++ frontend/src/login/Signup.tsx
@@ -1,7 +1,19 @@
 import { Header } from "../ui/Header.tsx";
 import { Footer } from "../ui/Footer.tsx";
 
+import { useState } from "react";
+
 export function Signup() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  function clearForm() {
+    setName("");
+    setEmail("");
+    setPassword("");
+  }
+
   return (
     <>
       <Header />
@@ -12,17 +24,25 @@ export function Signup() {
             <h3 className="login-text_field">Por favor crea tu usuario</h3>
 
             <input
+              type="text"
+              className="login-input_name"
+              required
+              placeholder="type your name"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
+
+            <input
               type="email"
               className="login-input_gmail"
               required
               placeholder="gmail"
-            />
-
-            <input
-              type="text"
-              className="login-input_number"
-              required
-              placeholder="cell phone"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
             />
 
             <input
@@ -30,15 +50,45 @@ export function Signup() {
               className="login-input_password"
               required
               placeholder="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
             />
           </div>
 
           <div className="login-buttons">
-            <button className="login-button_send create">Crear</button>
+            <button
+              className="login-button_send create"
+              onClick={() => {
+                sendDataUser(name, email, password);
+                clearForm();
+              }}
+            >
+              Crear
+            </button>
           </div>
         </div>
       </section>
       <Footer />
     </>
   );
+}
+
+function sendDataUser(name: string, email: string, password: string) {
+  console.log("Sending data...", name, email, password);
+
+  fetch("http://localhost:3000/register", {
+    method: "POST",
+
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+    body: JSON.stringify({
+      'name': name,
+      'email': email,
+      'password': password,
+    }),
+  });
 }
